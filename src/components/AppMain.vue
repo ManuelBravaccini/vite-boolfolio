459353015ebfa69bbb463353b73da087533e4c1b
ManuelBravaccini <manuelbravaccini2000@gmail.com>
2023-03-02 23:31:48 +0100

    Rearrenge scaffolding

---
+++ src/components/AppMain.vue
@@ -0,0 +1,34 @@
+<script>
+export default {
+    name: 'AppMain',
+    data() {
+        return {
+            project: [],
+            loading: false,
+            urlAddress: '',
+        }
+    },
+
+    methods: {
+
+    },
+
+    created() {
+
+    },
+}
+</script>
+
+<template>
+    <section>
+        <div class="container">
+            <div class="row">
+                <h1>Test</h1>
+            </div>
+        </div>
+    </section>
+</template>
+
+<style lang="">
+    
+</style>
